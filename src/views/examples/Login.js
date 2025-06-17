917c79c60e66b37b94d9c6b09c679bf651808b90
geraldineferreras <ferreras.geraldine@gmail.com>
2025-06-17 22:16:58 +0800

    Commit changes

--- src/views/examples/Login.js
+++ src/views/examples/Login.js
@@ -40,7 +40,7 @@ const Login = () => {
         <Card className="bg-secondary shadow border-0">
           <CardHeader className="bg-transparent pb-5">
             <div className="text-muted text-center mt-2 mb-3">
-              <small>Signnnnn in</small>
+              <small>Signnn in</small>
             </div>
             <div className="btn-wrapper text-center">
               <Button
