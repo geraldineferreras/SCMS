d49d78caa1d2a164d965f280013a133fb0f904df
geraldineferreras <ferreras.geraldine@gmail.com>
2025-06-17 11:58:25 +0800

    Initial commit: Argon Dashboard React project

---
+++ src/views/examples/Register.js
@@ -0,0 +1,155 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.2.4
+=========================================================
+
+=========================================================
+
+*/
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Row,
+  Col,
+} from "reactstrap";
+import { Link, useNavigate } from "react-router-dom";
+
+const Register = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <Col lg="5" md="7">
+        <Card className="bg-secondary shadow border-0">
+          <CardHeader className="bg-transparent pb-5">
+            <div className="text-center mt-2 mb-3">
+              <h2>Register</h2>
+            </div>
+            <div className="text-muted text-center mb-3">
+              <small>Sign up with</small>
+            </div>
+            <div className="btn-wrapper text-center">
+              <Button
+                className="btn-neutral btn-icon"
+                color="default"
+                onClick={() => navigate("/auth/qr-register")}
+              >
+                <span className="btn-inner--icon">
+                  <i className="ni ni-camera-compact" />
+                </span>
+                <span className="btn-inner--text">QR Code</span>
+              </Button>
+              <Button
+                className="btn-neutral btn-icon"
+                color="default"
+                href="#pablo"
+                onClick={(e) => e.preventDefault()}
+              >
+                <span className="btn-inner--icon">
+                  <img
+                    alt="..."
+                    src={
+                      require("../../assets/img/icons/common/google.svg")
+                        .default
+                    }
+                  />
+                </span>
+                <span className="btn-inner--text">Google</span>
+              </Button>
+            </div>
+          </CardHeader>
+          <CardBody className="px-lg-5 py-lg-5">
+            <div className="text-center text-muted mb-4">
+              <small>Or sign up with credentials</small>
+            </div>
+            <Form role="form">
+              <FormGroup>
+                <InputGroup className="input-group-alternative mb-3">
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="ni ni-hat-3" />
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input placeholder="Name" type="text" />
+                </InputGroup>
+              </FormGroup>
+              <FormGroup>
+                <InputGroup className="input-group-alternative mb-3">
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="ni ni-email-83" />
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input
+                    placeholder="Email"
+                    type="email"
+                    autoComplete="new-email"
+                  />
+                </InputGroup>
+              </FormGroup>
+              <FormGroup>
+                <InputGroup className="input-group-alternative">
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="ni ni-lock-circle-open" />
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input
+                    placeholder="Password"
+                    type="password"
+                    autoComplete="new-password"
+                  />
+                </InputGroup>
+              </FormGroup>
+              <div className="text-muted font-italic">
+                <small>
+                  password strength:{" "}
+                  <span className="text-success font-weight-700">strong</span>
+                </small>
+              </div>
+              <Row className="my-4">
+                <Col xs="12">
+                  <div className="custom-control custom-control-alternative custom-checkbox">
+                    <input
+                      className="custom-control-input"
+                      id="customCheckRegister"
+                      type="checkbox"
+                    />
+                    <label
+                      className="custom-control-label"
+                      htmlFor="customCheckRegister"
+                    >
+                      <span className="text-muted">
+                        I agree with the{" "}
+                        <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                          Privacy Policy
+                        </a>
+                      </span>
+                    </label>
+                  </div>
+                </Col>
+              </Row>
+              <div className="text-center">
+                <Button className="mt-4" color="primary" type="button">
+                  Create account
+                </Button>
+              </div>
+            </Form>
+          </CardBody>
+        </Card>
+      </Col>
+    </>
+  );
+};
+
+export default Register;
